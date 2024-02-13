c66340b8b87c2be2cbc63f391e06356c04a7a292
Your Name <rotiminicol117@gmail.com>
2024-02-13 01:02:49 -1100

    create header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='bg-pink shadow-md'>
+        <div className='flex justify-between item-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+        <h1 className='font-bold text sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-700'>NEW</span>
+            <span className='text-slate-500'>GREECE</span>
+        </h1>
+        </Link>
+        <form className='bg-slate-600 text-slate-50 p-3 rounded-lg flex items-center'>
+            <input 
+            type="text" 
+            placeholder='Search' 
+            className='bg-transparent focus:outline-none w-24 sm:w-64' />
+            <FaSearch className='text-slate-600'/>
+        </form>
+        <ul className='flex gap-4'>
+            <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:bg-slate-700'>Home</li>
+            </Link>
+            <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:bg-slate-700'>About</li>
+            </Link>
+            <Link to='/sign-in'>
+            <li className=' text-slate-700 hover:bg-slate-700'>SignIn</li>
+            </Link>
+    
+        </ul> 
+        </div> 
+    </header>
+  );
+};
